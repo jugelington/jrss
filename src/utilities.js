00fb40db6c1f077e200b55f2e87115823da9cfc8
jugelington <julianbeightondykes@gmail.com>
2019-01-26 20:25:40 +0000

    removed console log from utilities

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,6 @@
 import Parser from 'rss-parser';
 
 export const rssParser = async (feeds, feedName) => {
-  console.log(feeds);
   const parser = new Parser({
     customFields: {
       item: [['media:content', 'media:content', { keepArray: true }]],
