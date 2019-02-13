dbb695feccd1c06e29f636162a3d674585a0cc5b
jugelington <julianbeightondykes@gmail.com>
2019-02-13 17:34:45 +0000

    major rework of ui begun; modal removed; article cards no longer use bootstrap

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 // react-bootstrap
-import Card from 'react-bootstrap/lib/Card';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 // utilities
@@ -11,48 +10,56 @@ import '../CSS/cards.css';
 import '../CSS/loading-component.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
-const ArticleCard = ({
-  article,
-  article: { title, isoDate, link, source, content },
-  openModal,
-}) => {
-  const image = imageExtractor(article);
-  const parsedDate = new Date(isoDate);
-  const date = dateParser(parsedDate);
-  const time = timeParser(parsedDate);
+class ArticleCard extends Component {
+  state = {
+    expanded: false,
+  };
+  render() {
+    const { expanded } = this.state;
+    const {
+      article,
+      article: { title, isoDate, link, source, content },
+    } = this.props;
+    const image = imageExtractor(article);
+    const parsedDate = new Date(isoDate);
+    const date = dateParser(parsedDate);
+    const time = timeParser(parsedDate);
 
-  return (
-    <Card bg="dark" text="light" border="light" className="article-container">
-      <Card.Header className="article-container-header">
-        <div className="title-div">
-          <Card.Title className="article-container-title">{title}</Card.Title>
-        </div>
-        <div className="source-div">
-          <Card.Subtitle>
-            {`${time}   ${date}`} <br /> {source}
-          </Card.Subtitle>
-        </div>
-      </Card.Header>
-      <Card.Body className="article-card-body">
-        {image && <Card.Img className="article-container-image" src={image} />}
-        <Card.Text className="article-container-text article-card-text">
+    return (
+      <section className="article-card">
+        <section className="article-header">
+          {image && (
+            <img src={image} alt="none found" className="article-image" />
+          )}
+          <h5 className="article-title">{title}</h5>
+          <section className="article-subtitle">
+            <h6>{source}</h6>
+            <h6>{time + ' ' + date}</h6>{' '}
+          </section>
+
+          <ButtonGroup className="article-button-div">
+            <Button href={link} variant="outline-light">
+              Visit Site
+            </Button>
+            <Button onClick={this.toggleShow} variant="light">
+              {expanded ? 'Hide' : 'Expand'}
+            </Button>
+          </ButtonGroup>
+        </section>
+        <section className={expanded ? 'article-body' : 'hidden-body'}>
           {Parser(content, {
             replace: domNode => {
               if (domNode.name && domNode.name === 'img') return <></>;
             },
           })}
-        </Card.Text>
-        <ButtonGroup size="sm" className="article-button-div">
-          <Button href={link} variant="outline-secondary">
-            Read On Site
-          </Button>
-          <Button onClick={() => openModal(article)} variant="outline-light">
-            Read More
-          </Button>
-        </ButtonGroup>
-      </Card.Body>
-    </Card>
-  );
-};
+        </section>
+      </section>
+    );
+  }
+
+  toggleShow = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+}
 
 export default ArticleCard;
